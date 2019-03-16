ed0d0b1be583b0dcc4ec16ba552a954f484457b1
RunningIkkyu <389498623@qq.com>
2019-03-16 18:55:27 +0800

    update setting.py

--- proxypool/setting.py
+++ proxypool/setting.py
@@ -1,15 +1,43 @@
-
-
-# This url used is to test proxy.  change it to the website you want to crawl.
-TEST_URL = 'https://passport.weibo.cn/signin/login'
-
-
-# Database setting.
+# Redis数据库地址
 REDIS_HOST = '127.0.0.1'
-REDIS_PORT = 6379
-REDIS_PASSWORD = ''
-REDIS_KEY = 'proxy'
 
+# Redis端口
+REDIS_PORT = 6379
+
+# Redis密码，如无填None
+REDIS_PASSWORD = None
+
+REDIS_KEY = 'proxies'
+
+# 代理分数
 MAX_SCORE = 10
-MIN_SCORE = 1
-INITIAL_SCORE = 3
+MIN_SCORE = 0
+INITIAL_SCORE = 5
+
+VALID_STATUS_CODES = [200, 302]
+
+# 代理池数量界限
+POOL_UPPER_THRESHOLD = 50000
+
+# 检查周期
+TESTER_CYCLE = 20
+# 获取周期
+GETTER_CYCLE = 300
+
+# 测试API，建议抓哪个网站测哪个
+#TEST_URL = 'http://m.weibo.cn'
+TEST_URL = 'http://httpbin.org/get'
+#TEST_URL = 'https://passport.weibo.cn/signin/login'
+
+
+# API配置
+API_HOST = '0.0.0.0'
+API_PORT = 5555
+
+# 开关
+TESTER_ENABLED = True
+GETTER_ENABLED = True
+API_ENABLED = True
+
+# 最大批测试量
+BATCH_TEST_SIZE = 10

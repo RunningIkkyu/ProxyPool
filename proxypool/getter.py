d2deebf79693070231b0cbac71e65d04139866ae
RunningIkkyu <389498623@qq.com>
2019-03-18 22:28:02 +0800

    update getter

--- proxypool/getter.py
+++ proxypool/getter.py
@@ -1,7 +1,8 @@
 from proxypool.tester import Tester
 from proxypool.db import RedisClient
 from proxypool.crawler import Crawler
-from proxypool.setting import *
+from proxypool.setting import POOL_UPPER_THRESHOLD, PRIVATE_PROXY_ENABLE
+from proxypool.private_proxy import PrivateProxy
 import sys
 
 class Getter():
@@ -18,11 +19,17 @@ class Getter():
     
     def run(self):
         print('Getter started.')
-        if not self.is_over_threshold():
-            for callback_label in range(self.crawler.__CrawlFuncCount__):
-                callback = self.crawler.__CrawlFunc__[callback_label]
-                # 获取代理
-                proxies = self.crawler.get_proxies(callback)
-                sys.stdout.flush()
-                for proxy in proxies:
-                    self.redis.add(proxy)
+        if PRIVATE_PROXY_ENABLE:
+            proxies = PrivateProxy().get_proxies()
+            for proxy in proxies:
+                print('Add private proxy {}'.format(proxy))
+                self.redis.add(proxy)
+        else:
+            if not self.is_over_threshold():
+                for callback_label in range(self.crawler.__CrawlFuncCount__):
+                    callback = self.crawler.__CrawlFunc__[callback_label]
+                    # 获取代理
+                    proxies = self.crawler.get_proxies(callback)
+                    sys.stdout.flush()
+                    for proxy in proxies:
+                        self.redis.add(proxy)

30600e095a01dc16fdc67d8438487cb0367dff1f
RunningIkkyu <389498623@qq.com>
2019-03-18 17:15:19 +0800

    update tester.py

--- proxypool/tester.py
+++ proxypool/tester.py
@@ -1,9 +1,13 @@
 import sys
 import time
 import asyncio
+try:
+    from aiohttp import ClientError 
+except:     
+    from aiohttp import ClientProxyConnectionError as ProxyConnectionError
 import aiohttp
 from proxypool.db import RedisClient
-from proxypool.setting import TEST_URL, BATCH_TEST_SIZE
+from proxypool.setting import TEST_URL, BATCH_TEST_SIZE, VALID_STATUS_CODES
 
 
 class Tester(object):
@@ -20,26 +24,32 @@ class Tester(object):
             "Accept-Language": "zh-CN,zh;q=0.9",     
             "Host": "httpbin.org",
             "Upgrade-Insecure-Requests": "1",     
-            "User-Agent": "Mozilla/5.0 (WindowsNT 10.0; WOW64) "\
-                          "AppleWebKit/537.36 (KHTML, like Gecko) " \
-                          "Chrome/72.0.3626.121 Safari/537.36"   
+            "User-Agent": "Mozilla/5.0 (WindowsNT 10.0; WOW64)"\
+                          " AppleWebKit/537.36 (KHTML, like Gecko)" \
+                          " Chrome/72.0.3626.121 Safari/537.36"   
         }
 
-        async with aiohttp.ClientSession(headers=headers) as session:
-            real_proxy = 'http://' + proxy
-            print('Testing proxy: ', proxy)
+        conn = aiohttp.TCPConnector(verify_ssl=False)
+        async with aiohttp.ClientSession(connector=conn, headers=headers) as session:
+        #async with aiohttp.ClientSession(connector=conn) as session:
             try:
+                if isinstance(proxy, bytes):
+                    proxy = proxy.decode('utf-8')
+                real_proxy = 'http://' + proxy
+                print('Testing proxy: ', proxy)
                 async with session.get(TEST_URL, proxy=real_proxy, timeout=15,
                                        allow_redirects=False) as response:
-                    if response.status == 200:
+                    if response.status in  VALID_STATUS_CODES:
+                        print('Proxy', proxy, 'test success! ')
                         self.redis.max(proxy)
-                        print('Proxy test success: ', proxy)
                     else:
+                        print('Proxy', proxy, 'test failed! ')
                         self.redis.decrease(proxy)
-                        print('Proxy test failed: ', proxy)
+                        print('Proxy test failed: ', response.status, ' IP:', proxy)
             except Exception as e:
-                #self.redis.decrease(proxy)
-                print('Proxy connect failed: ', proxy)
+            #except (ClientError, aiohttp.client_exceptions.ClientConnectorError, asyncio.TimeoutError, AttributeError):
+                self.redis.decrease(proxy)
+                print('Proxy connected failed : ', e)
 
     def run(self):
         print('Proxy tester started.')
